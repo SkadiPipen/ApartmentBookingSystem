78a034614b337afab3264d9ff47c2cc5a538c9d3
Magdasal John Mark P. <magdasaljohnmark29@gmail.com>
2025-05-21 21:03:09 +0800

    Add files via upload

---
+++ app/Http/Controllers/USERSController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\USERS;
+use Illuminate\Http\Request;
+
+class USERSController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(USERS $uSERS)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(USERS $uSERS)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, USERS $uSERS)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(USERS $uSERS)
+    {
+        //
+    }
+}
